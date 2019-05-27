1ef0f78415f79c306cea0ff3d9106cd8ab0a69f2
DESKTOP-UPBE1VP\Spiri <spirids@live.de>
2019-05-27 21:20:27 +0700

    Finish App

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,58 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 
 void main() => runApp(
       MaterialApp(
-        home: null,
+        home: Scaffold(
+          backgroundColor: Colors.lightBlue[300],
+          appBar: AppBar(
+            title: Text(
+              'Ask Me Anything',
+            ),
+            backgroundColor: Colors.blueAccent[700],
+          ),
+          body: BallPage(),
+        ),
       ),
     );
+
+class BallPage extends StatefulWidget {
+  @override
+  _BallPageState createState() => _BallPageState();
+}
+
+class _BallPageState extends State<BallPage> {
+  int ballNo = 1;
+
+  void answer() {
+    ballNo = Random().nextInt(5) + 1;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            FlatButton(
+              highlightColor: Colors.lightBlue[300],
+              splashColor: Colors.lightBlue[300],
+              onPressed: () {
+                setState(() {
+                  answer();
+                });
+              },
+              child: Image.asset(
+                'images/ball$ballNo.png',
+                fit: BoxFit.contain,
+              ),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
